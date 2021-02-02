0fa792465c303be56fb8a39b2bbf97b0ba68e4b4
paarmin <parniaa@gmail.com>
2021-02-02 18:06:12 -0500

    data added to tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password) 
+VALUES 
+('Armin P', 'parniaa@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Arilin P', 'par@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Arnum P', 'armi@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u' );
